d7d6854d06959b02e9ebe6df7e8c1ac10dcbf6ff
Santhoshpsps <santhoshpsp4@gmail.com>
2023-03-10 07:50:01 +0530

    comeback

---
+++ DOM/index.js
@@ -0,0 +1,10 @@
+document.querySelector("h1").innerHTML = "Santhosh";
+//.querySelector("li a") -> a tag inside li tag
+//.querySelector("li.item") -> li tag with item class
+//                          -> IF MORE THAN ONE ITEM , RETURNS THE FIRST ELEMENT OF ARRAY OF ELEMENT SWIHT ITEM CLASS INSIDE LI TAG
+//.querySelector("#li img") -> img tag inside a li tag
+//.query SelectorAll("li.item") -> IF MORE THAN ONE ITEM ,RETURNS AN ARRAY OF ELEMENTS WITH ITEM CLASS INSIDE LI TAG
+//getElementById()	Returns the element that has the ID attribute with the specified value
+// getElementsByClassName()	Returns an HTMLCollection containing all elements with the specified class name
+// getElementsByName()	Returns an live NodeList containing all elements with the specified name
+// getElementsByTagName()	Returns an HTMLCollection containing all elements with the specified tag name
